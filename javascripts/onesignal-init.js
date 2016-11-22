b2f34479b055f504413eafe66be3966573037d8d
Jason Pang <jasonpang2011@gmail.com>
2016-11-22 15:51:43 -0800

    Create onesignal-init.js

---
+++ javascripts/onesignal-init.js
@@ -0,0 +1,11 @@
+window.OneSignal = window.OneSignal || [];
+var initOptions = {
+  appId: "fc6d5514-687f-4765-bd11-431b312b17fc",
+  autoRegister: true,
+  notifyButton: {
+    enable: true
+};
+
+OneSignal.push(function() {
+  OneSignal.init(initOptions);
+});
