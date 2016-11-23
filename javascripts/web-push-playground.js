7a6c8b568d2b85d53a4c8b1308f0ec8a310137ff
Jason Pang <jasonpang2011@gmail.com>
2016-11-22 16:23:03 -0800

    Create web-push-playground.js

---
+++ javascripts/web-push-playground.js
@@ -0,0 +1,68 @@
+function hookSendNotificationButton() {
+  document.querySelector('#button-send-notification').addEventListener('click') {
+    OneSignal.sendSelfNotification(
+      /* Title (defaults if unset) */
+      "OneSignal Web Push Notification",
+      /* Message (defaults if unset) */
+      "Action buttons increase the ways your users can interact with your notification.", 
+       /* URL (defaults if unset) */
+      'https://example.com/?_osp=do_not_open',
+      /* Icon */
+      'https://onesignal.com/images/notification_logo.png',
+      {
+        /* Additional data hash */
+        notificationType: 'news-feature'
+      }, 
+      [{ /* Buttons */
+        /* Choose any unique identifier for your button. The ID of the clicked button 			 is passed to you so you can identify which button is clicked */
+        id: 'like-button',
+        /* The text the button should display. Supports emojis. */
+        text: 'Like',
+        /* A valid publicly reachable URL to an icon. Keep this small because it's 				 downloaded on each notification display. */
+        icon: 'http://i.imgur.com/N8SN8ZS.png',
+        /* The URL to open when this action button is clicked. See the sections below 			 for special URLs that prevent opening any window. */
+        url: 'https://example.com/?_osp=do_not_open'
+      },
+      {
+        id: 'read-more-button',
+        text: 'Read more',
+        icon: 'http://i.imgur.com/MIxJp1L.png',
+        url: 'https://example.com/?_osp=do_not_open'
+      }]
+    );
+  }
+}
+
+function oneSignalStuff() {
+  OneSignal.push(function() {
+    showHideContent();
+    hookSendNotificationButton();
+  });
+}
+
+function showHideContent() {
+  OneSignal.isPushNotificationsEnabled()
+           .then(function(isEnabled) {
+             if (isEnabled) {
+               var elements = document.querySelectorAll('.show-when-subscribed');
+               elements.forEach(function(element) {    
+                 element.classList.remove("hidden");
+               });
+             } else {
+               var elements = document.querySelectorAll('.show-when-unsubscribed');
+               elements.forEach(function(element) {    
+                 element.classList.remove("hidden");
+               });
+             }
+           });
+}
+
+function run() {
+  oneSignalStuff();
+}
+
+if (document.readyState === "interactive" || document.readyState === "complete") {
+  run();
+} else {
+  window.addEventListener('DOMContentLoaded', run);
+}
