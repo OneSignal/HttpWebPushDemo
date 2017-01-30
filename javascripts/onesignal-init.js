90b4a320844d272bb24a9297c8a4ceabad89f75d
Jason Pang <jason@jasonpang.me>
2017-01-30 13:02:35 -0800

    Convert HTTPS web push demo to HTTP

--- javascripts/onesignal-init.js
+++ javascripts/onesignal-init.js
@@ -1,23 +1,13 @@
 window.OneSignal = window.OneSignal || [];
 var initOptions = {
-  appId: "fc6d5514-687f-4765-bd11-431b312b17fc",
+  appId: "12634044-7970-4268-8513-c679c2c6cee5",
+  subdomainName: 'onesignal-demo',
   autoRegister: true,
   notifyButton: {
     enable: true
   }
 };
 
-/*
-  Special Case: Worker files in a subdirectory
-  
-  We're hosting on GitHub pages so the worker files can't be uploaded to the root.
- */
-function changeServiceWorkerFilePath(initOptions) {
-  initOptions.path = '/WebPushDemo/';
-  OneSignal.SERVICE_WORKER_PARAM = {scope: '/WebPushDemo/' };
-};
-
 OneSignal.push(function() {
-  changeServiceWorkerFilePath(initOptions);
   OneSignal.init(initOptions);
 });
